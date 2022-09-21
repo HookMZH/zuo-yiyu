ba31d95ee3180d03570bdd81ed3bf4c601e86dd0
HookMZH <739067395@qq.com>
2022-09-21 23:46:27 +0800

    提交代码

---
+++ test_9_21/test_9_21/test.c
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+int MAX(int x, int y)
+{
+	if (x > y)
+		return x;
+	else
+		return y;
+}
+int main()
+{
+	int num1 = 10;
+	int num2 = 20;
+	int max = 0;
+	max = MAX(num1, num2);
+	printf("较大值是%d\n", max);
+	return 0;
+}
+//int main()
+//{
+//	int num1 = 10;
+//	int num2 = 20;
+//	if (num1 > num2)
+//		printf("较大值是:%d\n", num1);
+//	else
+//		printf("较大值是:%d\n", num2);
+//	return 0;
+//}
+//int main()
+//{
+//	printf("%d\n", strlen("c:\test\121"));
+//	return 0;
+//}
+//int main()
+//{
+//	//字符串的结束标志是：'\0'
+//	//'\0'-转义字符-ASCII值为0
+//	//0->数字0
+//	//'0'->ASCII值为48
+//	//EOF-end of file-文件结束标志- -1
+//	return 0;
+//}
